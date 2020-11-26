0f6c26dff33202ca9e19fc91418c8af1698092fb
isidoreduc <isidoreducasse79@gmail.com>
2020-11-26 02:59:39 +0100

    dockerized catalog api microservice

--- src/catalog/Catalog.API/Startup.cs
+++ src/catalog/Catalog.API/Startup.cs
@@ -1,25 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Catalog.API.Data;
 using Catalog.API.Data.Interfaces;
 using Catalog.API.Repositories;
 using Catalog.API.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace Catalog.API
 {
-    public class Startup
+  public class Startup
     {
         public Startup(IConfiguration configuration)
         {
