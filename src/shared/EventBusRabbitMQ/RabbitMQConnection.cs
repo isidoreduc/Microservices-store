6fdb565cf4474fe49285cf6fba77ec7d27433c1d
isidoreduc <isidoreducasse79@gmail.com>
2020-12-03 20:41:38 +0100

    setting rabbitmq connection

---
+++ src/shared/EventBusRabbitMQ/RabbitMQConnection.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+namespace EventBusRabbitMQ
+{
+  public class RabbitMQConnection : IRabbitMQConnection
+  {
+    private readonly IConnectionFactory _connectionFactory;
+    private IConnection _connection;
+    private bool _disposed;
+
+    public RabbitMQConnection(IConnectionFactory connectionFactory)
+    {
+      _connectionFactory = connectionFactory;
+      if(!IsConnected)
+        TryConnect();
+    }
+
+    public bool IsConnected => _connection != null && _connection.IsOpen && !_disposed;
+
+    public bool TryConnect()
+    {
+      try
+      {
+          _connection = _connectionFactory.CreateConnection();
+      }
+      catch (BrokerUnreachableException)
+      {
+          Thread.Sleep(2000);
+          _connection = _connectionFactory.CreateConnection();
+      }
+      return IsConnected == true ? true : false;
+    }
+
+    public IModel CreateModel()
+    {
+      return IsConnected == true ?
+        _connection.CreateModel() : throw new InvalidOperationException("No RabbitMq connection");
+    }
+
+    public void Dispose()
+    {
+        if(_disposed)
+            return;
+        else
+            _connection.Dispose();
+    }
+
+  }
+}
