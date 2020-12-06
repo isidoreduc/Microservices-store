3622911345d140418d27be03654675e6e6fa9ee2
isidoreduc <isidoreducasse79@gmail.com>
2020-12-06 13:44:27 +0100

    consume rabbitmq basket event in the order api

--- src/ordering/Ordering.API/Startup.cs
+++ src/ordering/Ordering.API/Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using AutoMapper;
+using EventBusRabbitMQ;
+using EventBusRabbitMQ.Producer;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -15,10 +17,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.API.ExtensionMethods;
+using Ordering.API.RabbitMQ;
 using Ordering.APPLICATION.Handlers;
 using Ordering.CORE.Repositories;
 using Ordering.INFRASTRUCTURE.Data;
 using Ordering.INFRASTRUCTURE.Repositories;
+using RabbitMQ.Client;
 
 namespace Ordering.API
 {
@@ -36,16 +41,33 @@ namespace Ordering.API
         {
             services.AddAutoMapper(typeof(Startup));
             services.AddMediatR(typeof(CheckoutOrderHandler).GetTypeInfo().Assembly);
-            services.AddTransient<IOrderRepository, OrderRepository>();
-            // for generic type injection
-            services.AddScoped(typeof(IRepositoryBase<>), typeof(Repository<>));
+            // services.AddTransient<IOrderRepository, OrderRepository>();
+            // // for generic type injection
+            // services.AddScoped(typeof(IRepositoryBase<>), typeof(Repository<>));
+            // we made singleton this in order to resolve in mediatR when consuming Rabbit
+            services.AddDbContext<OrderContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:MSSqlServer"]), ServiceLifetime.Singleton);
 
-            services.AddDbContext<OrderContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:MSSqlServer"]));
+            services.AddScoped(typeof(IRepositoryBase<>), typeof(Repository<>));
+            services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
+            services.AddTransient<IOrderRepository, OrderRepository>(); // we made transient this in order to resolve in mediatR when consuming Rabbit
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
             });
+
+            // rabbitmq connection
+            services.AddSingleton<IRabbitMQConnection>(sp =>
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = Configuration["EventBus:HostName"],
+                    UserName = Configuration["EventBus:UserName"],
+                    Password = Configuration["EventBus:Password"]
+                };
+                return new RabbitMQConnection(factory);
+            });
+            services.AddSingleton<EventRabbitMQConsumer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -68,6 +90,8 @@ namespace Ordering.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseRabbitMQListener();
         }
     }
 }
