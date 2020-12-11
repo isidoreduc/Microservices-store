e654d1bc3ede0bafe444b51c3e87fe7f6fafdb2d
isidoreduc <isidoreducasse79@gmail.com>
2020-12-11 17:05:53 +0100

    api gateway with ocelot + dockerization

--- src/ordering/Ordering.API/Controllers/OrderController.cs
+++ src/ordering/Ordering.API/Controllers/OrderController.cs
@@ -11,7 +11,7 @@ using Ordering.APPLICATION.Responses;
 namespace Ordering.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/v1/[controller]")]
     public class OrderController : ControllerBase
     {
         private readonly IMediator _mediator;
